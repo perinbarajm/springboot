d363752fdab8d4eed8a0e4f836bc34d4dcda4e73
perinbaraj.m <perinbaraj.m>
2024-05-21 10:55:00 +0530

    spring boot template

---
+++ src/main/java/com/ics/IcsApplicationStarter.java
@@ -0,0 +1,12 @@
+package com.ics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IcsApplicationStarter{
+	public static void main(String args[]) {
+		SpringApplication.run(IcsApplicationStarter.class, args);
+	}
+}
